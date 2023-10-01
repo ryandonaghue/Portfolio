4b76e1cdda70a07e3a23f17799b6ba3a7f8c2167
ryandonaghue <ryandonaghue@outlook.com>
2023-10-01 12:56:49 +0100

    Started Building Skills Component

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,5 +1,18 @@
+import { BadgeCheckIcon, ChipIcon } from "@heroicons/react/solid";
 import React from "react";
+import { skills } from "../data";
 
 export default function Skills(){
-    
+    return (
+        <section id="skills">
+            <div className="container px-5 py-10 mx-auto">
+                <div className="text-center mb-20">
+                    <ChipIcon className="w-10 inline-block mb-4" />
+                    <h1 className="sm:text-4xl text-3xl font-medium titledont text-white mb-4">
+                        Skills &amp; Technologies
+                    </h1>
+                </div>
+            </div>
+        </section>
+    )
 }
