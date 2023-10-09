8efa0aba4c317f9e9535a561fb3c824ac1faa6de
ryandonaghue <ryandonaghue@outlook.com>
2023-10-09 06:54:20 +0100

    Started Working in Testimonial Component

--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -1,5 +1,13 @@
 import React from "react";
+import { TerminalIcon, UserIcon } from "@heroicons/react/solid";
+import { testimonials } from "../data";
 
-export default function Testimonials(){
-    
+export default function Testimonials() {
+    return(
+        <section id="testimonials">
+            <div className="container px-5 py-10 mx-auto text-center">
+                <UsersIcon className="w-10 incline-block mb-4" />
+            </div>
+        </section>
+    )
 }
