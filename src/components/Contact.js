73c234e01fd87b2b6b83f32e95151d48d84091ba
ryandonaghue <ryandonaghue@outlook.com>
2023-10-29 14:02:22 +0000

    Continued Building Contact Component

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -16,8 +16,42 @@ export default function Contact() {
                         style={{filter: "opacity(0.7)"}}
                         src="https://www.google.com/maps/embed/v1/place?q=97+warren+st+new+york+city&key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8"
                     />
+                    <div className="bg-gray-900 relative flex flex-wrap py-6 rounded shadow-md">
+                        <div className="lg:w-1/2 px-6">
+                            <h2 className="title-font font-semibold text-white tracking-widest text-xs">
+                                ADDRESS
+                            </h2>
+                            <p className="mt-1">
+                                97 Warren St. <br />
+                                New York, NY 10007
+                            </p>
+                        </div>
+                        <div className="lg:w-1/2 px-6 mt-4 lg:mt-0">
+                            <h2 className="title-font font-semibold text-white tracking-widest text-xs">
+                                EMAIL
+                            </h2>
+                            <a className="text-indigo-400 leading-relaxed">
+                                reedbarger@email.com
+                            </a>
+                            <h2 className="title-font font-semibold text-white tracking-widest text-xs mt-4">
+                                PHONE
+                            </h2>
+                            <p className="leading-relaxed">+447568 086611</p>
+                        </div>
+                    </div>
                 </div>
-
+                <form 
+                netlify
+                name="contact"
+                className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
+                <h2 className="text-white sm:text=4xl text-3xl mb-1 font-medium title-font">
+                    'Hire Me'
+                </h2>
+                <p className="leading-relaxed mb-5">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illum
+            suscipit officia aspernatur veritatis. Asperiores, aliquid?
+                </p>
+                </form>
             </div>
 
         </section>
